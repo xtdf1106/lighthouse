656acceb789b40738c965c421d39577de78ae893
Shane Exterkamp <shaneexterkamp5@gmail.com>
2019-06-03 13:23:31 -0700

    basic format

--- lighthouse-core/audits/byte-efficiency/unminified-css.js
+++ lighthouse-core/audits/byte-efficiency/unminified-css.js
@@ -16,6 +16,21 @@ const UIStrings = {
   /** Description of a Lighthouse audit that tells the user *why* they should minify (remove whitespace) the page's CSS code. This is displayed after a user expands the section to see more. No character length limits. 'Learn More' becomes link text to additional documentation. */
   description: 'Minifying CSS files can reduce network payload sizes. ' +
   '[Learn more](https://developers.google.com/web/tools/lighthouse/audits/minify-css).',
+  titleObj: {
+    message: 'Minifying CSS files can reduce network payload sizes.' +
+      ' $LINK_START$Learn More$LINK_END$',
+    description: 'Description of a Lighthouse audit that tells the user *why* they should ' +
+      'minify (remove whitespace) the page\'s CSS code. This is displayed after a user expands ' +
+      'the section to see more. No character length limits.',
+    placeholders: {
+      link_start: {
+        content: '[',
+      },
+      link_end: {
+        content: '](https://developers.google.com/web/tools/lighthouse/audits/minify-css)',
+      },
+    },
+  },
 };
 
 const str_ = i18n.createMessageInstanceIdFn(__filename, UIStrings);
